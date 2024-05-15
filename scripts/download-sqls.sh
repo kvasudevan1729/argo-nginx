d5962faaf361b7490066a63e9944e41c346cc91b
Krish Vasudevan <164563749+kvasudevan1729@users.noreply.github.com>
2024-05-15 12:50:00 +0100

    Clean up sqls (#14)
    * Clean up unused sql data files
    
    * Clean up unused sql data files

--- scripts/download-sqls.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl -O https://raw.githubusercontent.com/kvasudevan1729/flask-postres-perf-test/main/sql/01-create-tables.sql
-curl -O https://raw.githubusercontent.com/kvasudevan1729/flask-postres-perf-test/main/sql/02-department-data.sql
-curl -O https://raw.githubusercontent.com/kvasudevan1729/flask-postres-perf-test/main/sql/03-employees-data.sql
-curl -O https://raw.githubusercontent.com/kvasudevan1729/flask-postres-perf-test/main/sql/04-rooms-data.sql
-curl -O https://raw.githubusercontent.com/kvasudevan1729/flask-postres-perf-test/main/sql/05-employee-room-data.sql
-curl -O https://raw.githubusercontent.com/kvasudevan1729/flask-postres-perf-test/main/sql/06-desktop-data.sql

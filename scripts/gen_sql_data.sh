103b0643d28a8da7c121a2def53e524176ab217e
Krish Vasudevan <164563749+kvasudevan1729@users.noreply.github.com>
2024-05-23 09:40:37 +0100

    Modify db sqls and timeout settings (#15)
    * Set the namespace
    
    * Add more tables
    
    * Tweak probe and timeout settings

--- scripts/gen_sql_data.sh
+++ scripts/gen_sql_data.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 dept_sql="./02-department-data.sql"
 emp_sql="./03-employees-data.sql"
 rooms_sql="./04-rooms-data.sql"
 emp_room_sql="./05-employee-room-data.sql"
 desktop_sql="./06-desktop-data.sql"
+skill_sql="./07-skill-data.sql"
+skill_level_sql="./07-skill-level-data.sql"
+emp_skills_sql="./08-employee-skills-data.sql"
 departments=""
 
 echo "=> generating sql for department table ..."
@@ -21,33 +24,88 @@ school=$(tr -dc 'A-Z' < /dev/urandom | head -c4)
 departments="${departments} ${dept}"
 echo "  ('${dept}', '${school}');" >> ${dept_sql}
 
+#### SKILL_LEVEL
+skill_level_cnt=10
+skill_levels=()
+echo "=> generating sql for skill_level table ..."
+echo "INSERT INTO skill_level (level_name, description) VALUES" > ${skill_level_sql}
+for i in $(seq 1 $skill_level_cnt); do
+    level_name=$(tr -dc 'A-Z' < /dev/urandom | head -c10)
+    description=$(tr -dc 'A-Z' < /dev/urandom | head -c20)
+    skill_levels+=("${level_name}")
+    echo "  ('${level_name}', '${description}')," >> ${skill_level_sql}
+done
+
+level_name=$(tr -dc 'A-Z' < /dev/urandom | head -c10)
+description=$(tr -dc 'A-Z' < /dev/urandom | head -c20)
+skill_levels+=("${level_name}")
+echo "  ('${level_name}', '${description}');" >> ${skill_level_sql}
+
+#### SKILL
+
+skills_cnt=50
+skills=()
+echo "=> generating sql for skill table ..."
+echo "INSERT INTO skill (name, description) VALUES" > ${skill_sql}
+for i in $(seq 1 $skills_cnt); do
+    skill_name=$(tr -dc 'A-Z' < /dev/urandom | head -c10)
+    description=$(tr -dc 'A-Z' < /dev/urandom | head -c20)
+    skills+=("${skill_name}")
+    echo "  ('${skill_name}', '${description}')," >> ${skill_sql}
+done
+
+skill_name=$(tr -dc 'A-Z' < /dev/urandom | head -c10)
+description=$(tr -dc 'A-Z' < /dev/urandom | head -c20)
+skills+=("${skill_name}")
+echo "  ('${skill_name}', '${description}');" >> ${skill_sql}
+
+### ALL OTHER TABLESD
+
 echo "=> generating sql for remaining tables ..."
-echo "INSERT INTO employee (name, age, department_name) VALUES" > ${emp_sql}
+echo "INSERT INTO employee (id, name, age, department_name) VALUES" > ${emp_sql}
 echo "INSERT INTO rooms (name, department) VALUES" > ${rooms_sql}
 echo "INSERT INTO employee_room (room_name, employee_id) VALUES" > ${emp_room_sql}
 echo "INSERT INTO desktop (name, employee_id, desktop_type) VALUES" > ${desktop_sql}
+echo "INSERT INTO employee_skills (employee_id, skill_name, skill_level) VALUES" > ${emp_skills_sql}
 
+no_recs_per_dept=1000
 for d in ${departments}; do
-    for i in $(seq 1 1000); do
+  echo -e "\n=> department: ${d}"
+    for i in $(seq 1 ${no_recs_per_dept}); do
         printf "."
+        emp_id=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20);
         emp_name=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20);
-        emp_age=$(tr -dc '0-9' < /dev/urandom | head -c8);
-        echo "  ('${emp_name}', ${emp_age}, '${d}')," >> ${emp_sql}
+        emp_age=$(tr -dc '0-9' < /dev/urandom | head -c2);
+        echo "  ('${emp_id}', '${emp_name}', ${emp_age}, '${d}')," >> ${emp_sql}
+
         room=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20)
         echo "  ('${room}', '${d}')," >> ${rooms_sql}
-        echo "  ('${room}', $i)," >> ${emp_room_sql}
+        echo "  ('${room}', '${emp_id}')," >> ${emp_room_sql}
+
         desktop_win=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20)
         desktop_linux=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20)
-        echo "  ('${desktop_win}', ${i}, 'windows')," >> ${desktop_sql}
-        echo "  ('${desktop_linux}', ${i}, 'linux')," >> ${desktop_sql}
+        echo "  ('${desktop_win}', '${emp_id}', 'windows')," >> ${desktop_sql}
+        echo "  ('${desktop_linux}', '${emp_id}', 'linux')," >> ${desktop_sql}
+
+        # emp skills
+        skill_name="${skills[$RANDOM%${skills_cnt}]}"
+        skill_level="${skill_levels[$RANDOM%${skill_level_cnt}]}"
+        echo "  ('${emp_id}', '${skill_name}', '${skill_level}')," >> ${emp_skills_sql}
     done
 done
 
+emp_id=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20);
 emp_name=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20);
 emp_age=$(tr -dc '0-9' < /dev/urandom | head -c8);
-echo "  ('${emp_name}', ${emp_age}, '${dept}');" >> ${emp_sql}
+echo "  ('${emp_id}', '${emp_name}', ${emp_age}, '${dept}');" >> ${emp_sql}
+
 room=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20)
 echo "  ('${room}', '${dept}');" >> ${rooms_sql}
-echo "  ('${room}', 1);" >> ${emp_room_sql}
+echo "  ('${room}', '${emp_id}');" >> ${emp_room_sql}
+
 desktop_win=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c20)
-echo "  ('${desktop_win}', 1, 'windows');" >> ${desktop_sql}
+echo "  ('${desktop_win}', '${emp_id}', 'windows');" >> ${desktop_sql}
+
+skill_name="${skills[$RANDOM%${skills_cnt}]}"
+skill_level="${skill_levels[$RANDOM%${skill_level_cnt}]}"
+echo "  ('${emp_id}', '${skill_name}', '${skill_level}');" >> ${emp_skills_sql}
--- sql/01-create-tables.sql
+++ sql/01-create-tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE employee (
-  id SERIAL PRIMARY KEY,
+  id varchar(20) PRIMARY KEY,
   name VARCHAR(100),
   age INTEGER,
   department_name VARCHAR(30),
@@ -23,7 +23,7 @@ CREATE TABLE rooms (
 
 CREATE TABLE employee_room (
   room_name VARCHAR(20),
-  employee_id INTEGER,
+  employee_id VARCHAR(20),
   PRIMARY KEY(room_name, employee_id),
   CONSTRAINT fk_employee_room
     FOREIGN KEY(room_name)
@@ -36,9 +36,35 @@ CREATE TABLE employee_room (
 CREATE TABLE desktop (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30),
-  employee_id INTEGER,
+  employee_id VARCHAR(20),
   desktop_type VARCHAR(10),
   CONSTRAINT fk_employee
     FOREIGN KEY(employee_id)
       REFERENCES employee(id)
+);
+
+CREATE TABLE skill (
+  name VARCHAR(10) PRIMARY KEY,
+  description VARCHAR(20)
+);
+
+CREATE TABLE skill_level (
+  level_name VARCHAR(10) PRIMARY KEY,
+  description VARCHAR(20)
+);
+
+CREATE TABLE employee_skills (
+  employee_id VARCHAR(20),
+  skill_name VARCHAR(20),
+  skill_level VARCHAR(10),
+  PRIMARY KEY(employee_id, skill_name),
+  CONSTRAINT fk_employee_skills_employee
+    FOREIGN KEY(employee_id)
+      REFERENCES employee(id),
+  CONSTRAINT fk_employee_skills_skill
+    FOREIGN KEY(skill_name)
+      REFERENCES skill(name),
+  CONSTRAINT fk_employee_skills_level
+    FOREIGN KEY(skill_level)
+      REFERENCES skill_level(level_name)
 );
